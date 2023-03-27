a4f6e2366996fe23bae6646f8891cb9357f29e39
achchuthan-suntharalingam <achchu0103@gmail.com>
2023-03-27 11:49:31 +0530

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express ();
+const app = express();
+const Note = require('./models/note');
+const notesRouter = require('./routes/notes');
+const methodOverride = require('method-override');
+require('dotenv').config();
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server Has Started`);
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+app.get('/', async (req, res) => {
+  const notes = await Note.find().sort('-createdAt');
+  res.render('index', { notes: notes });
 });
 
-app.get('/', (req, res) => {
-    res.send(`Yayy! It's working`);
+mongoose.connect(process.env.SERVER, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(()=>{
+  console.log('Database Connected Successfully!');
+});
+
+app.use('/', notesRouter);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server Has Started!`);
 });
